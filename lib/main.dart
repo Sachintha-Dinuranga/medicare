d00053d3e9f1be446e343cc6a8108fa9b08c5a61
Ruvindi Chathurya <133088416+MatharaArachchi@users.noreply.github.com>
2024-10-07 19:38:20 +0530

    patient acc created
    adding login, signup, patient account

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:medicare/features/reminder/home.dart';
+import 'package:medicare/features/medical/Screens/login_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -12,7 +16,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Home(),
+      home: LoginScreen(),
     );
   }
 }
