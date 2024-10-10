85d5efb0b01fd04d2df76315aa9955ac6b6c717f
Praboth Sandaruwan <bleezbub12@gmail.com>
2024-10-10 15:21:45 +0530

    cmm rt

--- lib/features/medical/Screens/home_screen.dart
+++ lib/features/medical/Screens/home_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:medicare/features/medical/Screens/patient_details_screen.dart';
 import 'package:medicare/features/medical/Screens/view_patient_details.dart';
+import 'package:medicare/features/reminder/reminder_list.dart';
+import 'package:medicare/features/map_feature/screens/first_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -34,8 +36,8 @@ class HomeScreen extends StatelessWidget {
               ),
             ),
             ListTile(
-              leading: const Icon(Icons.person),
-              title: const Text('Patient Details'),
+              leading: const Icon(Icons.person_add),
+              title: const Text('Add Patient Details'),
               onTap: () {
                 // Navigate to PatientDetailsScreen
                 Navigator.push(
@@ -46,7 +48,7 @@ class HomeScreen extends StatelessWidget {
               },
             ),
             ListTile(
-              leading: const Icon(Icons.person_search),
+              leading: const Icon(Icons.person_rounded),
               title: const Text('View Patient Details'),
               onTap: () {
                 // Navigate to PatientDetailsScreen
@@ -55,6 +57,35 @@ class HomeScreen extends StatelessWidget {
                   MaterialPageRoute(
                       builder: (context) => const ViewPatientDetailsScreen()),
                 );
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.timelapse),
+              title: const Text('Reminders'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const ReminderList()),
+                );
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.map),
+              title: const Text('Map'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const FirstScreen()),
+                );
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.sos),
+              title: const Text('SOS'),
+              onTap: () {
+                Navigator.of(context).pushReplacementNamed('/sos');
               },
             ),
             ListTile(
