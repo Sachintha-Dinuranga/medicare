4a2c3fe10a25d6922f4f8be7a9715922887931e2
Praboth Sandaruwan <bleezbub12@gmail.com>
2024-10-12 07:06:47 +0530

    cmm

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
 import 'package:medicare/features/reminder/notification_service.dart';
 import 'package:medicare/features/medical/Screens/login_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -21,21 +20,6 @@ void main() async {
   }
 
   runApp(const MyApp());
-=======
-import 'package:ueehe/screens/edit_profile_screen.dart';
-import 'package:ueehe/screens/essential_info_form_screen.dart';
-import 'package:ueehe/screens/sos_screen.dart';
-import 'package:ueehe/screens/splash_screen.dart';
-import 'package:ueehe/screens/welcome_screen.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:ueehe/screens/EmergencyContactsScreen.dart';
-
-/// The main entrypoint for the application. It initializes the Flutter engine
-/// and runs the [MyApp] widget.
-void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  runApp(MyApp());
->>>>>>> origin/hasindu
 }
 
 class MyApp extends StatelessWidget {
@@ -43,24 +27,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-<<<<<<< HEAD
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: LoginScreen(),
-=======
-    return MaterialApp(
-      title: 'Emergency App',
-      theme: ThemeData(textTheme: GoogleFonts.interTextTheme()),
-      initialRoute: '/',
-      routes: {
-        '/': (context) => SplashScreen(),
-        '/welcome': (context) => WelcomeScreen(),
-        '/essentialForm': (context) => EssentialInfoFormScreen(),
-        '/sos': (context) => SosScreen(),
-        '/editProfile': (context) => EditProfileScreen(),
-        '/emegencyContacts': (context) => EmergencyContactsScreen(),
-      },
->>>>>>> origin/hasindu
     );
   }
 }
